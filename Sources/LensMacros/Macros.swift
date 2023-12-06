0b56733805582a8f24363e93e6141c7743fe58d1
Valery Kokanov <illigrium@gmail.com>
2023-12-06 06:27:40 +0300

    Fixing macro attaching policy

--- Sources/LensMacros/Macros.swift
+++ Sources/LensMacros/Macros.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-@attached(member, names: named(AllLenses), named(allLenses), named(init))
+@attached(member, names: named(AllLenses), named(allLenses), named(init), arbitrary)
 public macro AutoLens() =
   #externalMacro(
     module: "LensMacrosPlugin", type: "AutoLensMacro"
